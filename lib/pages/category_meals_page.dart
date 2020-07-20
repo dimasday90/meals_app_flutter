291f44a8413c3bdc99a449fe1868c6bb2abea195
Dimas Agung Yudono <dimasday90@gmail.com>
2020-07-20 23:46:05 +0700

    dummy data

---
+++ lib/pages/category_meals_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../categories_meals_data.dart';
+import '../widgets/meal_item.dart';
+
+class CategoryMealsPage extends StatelessWidget {
+  static const routeName = '/category-meals';
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    // final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return MealItem(
+              id: categoryMeals[index].id,
+              title: categoryMeals[index].title,
+              duration: categoryMeals[index].duration,
+              imageUrl: categoryMeals[index].imageUrl,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability,
+            );
+          },
+          itemCount: categoryMeals.length,
+        ),
+      ),
+    );
+  }
+}
