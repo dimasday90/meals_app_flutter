0c9cad97ff8a44026c532c2f2f82dcc62856bd43
Dimas Agung Yudono <dimasday90@gmail.com>
2020-07-20 23:47:32 +0700

    main appBar, configure tab and drawer

---
+++ lib/pages/tabs_pages.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/main_drawer.dart';
+
+import './favorites_page.dart';
+import './categories_page.dart';
+
+class TabsPage extends StatefulWidget {
+  @override
+  _TabsPageState createState() => _TabsPageState();
+}
+
+class _TabsPageState extends State<TabsPage> {
+  List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesPage(),
+      'title': 'Meals',
+    },
+    {
+      'page': FavoritesPage(),
+      'title': 'Favorites',
+    },
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData mainTheme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      drawer: MainDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: mainTheme.accentColor,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favorites'),
+          )
+        ],
+        backgroundColor: mainTheme.primaryColor,
+      ),
+    );
+  }
+}
