5a56da554be69f9fed05a8adfad203d573a917eb
Dimas Agung Yudono <dimasday90@gmail.com>
2020-07-21 23:24:06 +0700

    wrapped text widget in the Center widget

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -26,7 +26,8 @@ class CategoryItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(15),
       child: Container(
         padding: const EdgeInsets.all(15),
-        child: Text(title, style: Theme.of(context).textTheme.headline6),
+        child: Center(
+            child: Text(title, style: Theme.of(context).textTheme.headline6)),
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
