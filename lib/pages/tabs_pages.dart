bd332b24db159c68b51064060477a59e7b19ebba
Dimas Agung Yudono <dimasday90@gmail.com>
2020-07-21 23:25:38 +0700

    received favorited meals list to be initiated

--- lib/pages/tabs_pages.dart
+++ lib/pages/tabs_pages.dart
@@ -5,23 +5,37 @@ import '../widgets/main_drawer.dart';
 import './favorites_page.dart';
 import './categories_page.dart';
 
+import '../models/meal.dart';
+
 class TabsPage extends StatefulWidget {
+  final List<Meal> favoritedMeals;
+
+  TabsPage({this.favoritedMeals});
+
   @override
   _TabsPageState createState() => _TabsPageState();
 }
 
 class _TabsPageState extends State<TabsPage> {
-  List<Map<String, Object>> _pages = [
-    {
-      'page': CategoriesPage(),
-      'title': 'Meals',
-    },
-    {
-      'page': FavoritesPage(),
-      'title': 'Favorites',
-    },
-  ];
+  List<Map<String, Object>> _pages;
   int _selectedPageIndex = 0;
+
+  @override
+  void initState() {
+    _pages = [
+      {
+        'page': CategoriesPage(),
+        'title': 'Meals',
+      },
+      {
+        'page': FavoritesPage(
+          favoritedMeals: widget.favoritedMeals,
+        ),
+        'title': 'Favorites',
+      },
+    ];
+    super.initState();
+  }
 
   void _selectPage(int index) {
     setState(() {
@@ -49,7 +63,7 @@ class _TabsPageState extends State<TabsPage> {
             title: Text('Categories'),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
+            icon: Icon(Icons.star),
             title: Text('Favorites'),
           )
         ],
