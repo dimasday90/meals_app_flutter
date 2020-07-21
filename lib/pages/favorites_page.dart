45b31b9f6de0af5a95012c18fd0be6345c289455
Dimas Agung Yudono <dimasday90@gmail.com>
2020-07-21 23:20:48 +0700

    finished building favorites page

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -1,16 +1,66 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/meal_item.dart';
+
+import '../models/meal.dart';
+
 class FavoritesPage extends StatefulWidget {
+  final List<Meal> favoritedMeals;
+
+  FavoritesPage({this.favoritedMeals});
+
   @override
   _FavoritesPageState createState() => _FavoritesPageState();
 }
 
 class _FavoritesPageState extends State<FavoritesPage> {
+  List<Meal> _favorites = [];
+  // bool _loadedInitData = false;
+
+  @override
+  void initState() {
+    _favorites = widget.favoritedMeals;
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _favorites = widget.favoritedMeals;
+    super.didChangeDependencies();
+  }
+
+  Widget _buildEmptyFavorites() {
+    return Center(
+      child: Text(
+        'No favorites available - start adding some!',
+        style: Theme.of(context).textTheme.headline6,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget _buildFavoriteMealsList() {
+    return ListView.builder(
+      itemBuilder: (ctx, index) {
+        return MealItem(
+          id: _favorites[index].id,
+          title: _favorites[index].title,
+          duration: _favorites[index].duration,
+          imageUrl: _favorites[index].imageUrl,
+          complexity: _favorites[index].complexity,
+          affordability: _favorites[index].affordability,
+        );
+      },
+      itemCount: _favorites.length,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Text('favorites page'),
-    ));
+    return SafeArea(
+      child: _favorites.isEmpty
+          ? _buildEmptyFavorites()
+          : _buildFavoriteMealsList(),
+    );
   }
 }
