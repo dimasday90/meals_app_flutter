12d282aa78a4c0427b809987114bd8acaddd289d
Dimas Agung Yudono <dimasday90@gmail.com>
2020-08-06 08:36:08 +0700

    finish beta learning

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# meals_app
+# Day Meal App
 
-A new Flutter project.
+Meals App provides various meals with its own recipe to follow.
 
-## Getting Started
+## Run The App
 
-This project is a starting point for a Flutter application.
+- Clone this repo
+- Run Android emulator or use real Android device
+- Run the command and find your favorite meal :smile:
+  ```
+  flutter run
+  ```
 
-A few resources to get you started if this is your first Flutter project:
+## Credits
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+[Meal Icon](https://icons8.com/icons/set/meal) by [Icons8](https://icons8.com)
--- lib/pages/meal_detail_page.dart
+++ lib/pages/meal_detail_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../models/meal.dart';
+
 import '../categories_meals_data.dart';
 
 class MealDetailPage extends StatelessWidget {
@@ -11,6 +13,62 @@ class MealDetailPage extends StatelessWidget {
     this.toggleFavorite,
     this.isFavorite,
   });
+
+  Widget buildSliverAppBar(
+      MediaQueryData mediaQuery, String mealId, Meal selectedMeal) {
+    return SliverAppBar(
+      expandedHeight: mediaQuery.size.height * 0.33,
+      floating: false,
+      pinned: true,
+      snap: false,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Text(
+          selectedMeal.title,
+          overflow: TextOverflow.fade,
+          softWrap: true,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        background: Image.network(
+          selectedMeal.imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            isFavorite(mealId) ? Icons.star : Icons.star_border,
+          ),
+          onPressed: () => toggleFavorite(mealId),
+          tooltip: 'Add to favorites',
+        ),
+      ],
+    );
+  }
+
+  Widget buildSliverGrid(List<Widget> list) {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return Card(
+            elevation: 4,
+            child: Container(
+              alignment: Alignment.center,
+              child: list[index],
+            ),
+          );
+        },
+        childCount: list.length,
+      ),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 300,
+        childAspectRatio: 6 / 3,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
 
   Widget buildSliverSubHeadline(ThemeData mainTheme, String title) {
     return SliverToBoxAdapter(
@@ -53,48 +111,113 @@ class MealDetailPage extends StatelessWidget {
     );
   }
 
+  String complexityText(Complexity complexity) {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Hard:
+        return 'Hard';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String affordabilityText(Affordability affordability) {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Pricey:
+        return 'Pricey';
+      case Affordability.Luxurious:
+        return 'Luxurious';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  List<String> mealTypes(Meal meal) {
+    List<String> result = [];
+    if (meal.isGlutenFree) {
+      result.add('Glutten-free');
+    }
+    if (meal.isLactoseFree) {
+      result.add('Lactose-free');
+    }
+    if (meal.isVegan) {
+      result.add('Vegan');
+    }
+    if (meal.isVegetarian) {
+      result.add('Vegetarian');
+    }
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     final MediaQueryData mediaQuery = MediaQuery.of(context);
     final ThemeData mainTheme = Theme.of(context);
     final mealId = ModalRoute.of(context).settings.arguments as String;
     final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
-    final sliverAppBar = SliverAppBar(
-      expandedHeight: mediaQuery.size.height * 0.33,
-      floating: false,
-      pinned: true,
-      snap: false,
-      flexibleSpace: FlexibleSpaceBar(
-        title: Text(
-          selectedMeal.title,
-          overflow: TextOverflow.fade,
-          softWrap: true,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+    final List<Widget> detailContents = [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(Icons.schedule),
+          SizedBox(
+            width: 10,
           ),
-        ),
-        background: Image.network(
-          selectedMeal.imageUrl,
-          fit: BoxFit.cover,
-        ),
+          Text('${selectedMeal.duration} min')
+        ],
       ),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(
-            isFavorite(mealId) ? Icons.star : Icons.star_border,
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(Icons.work),
+          SizedBox(
+            width: 10,
           ),
-          onPressed: () => toggleFavorite(mealId),
-          tooltip: 'Add to favorites',
-        ),
-      ],
-    );
+          Text(
+            complexityText(
+              selectedMeal.complexity,
+            ),
+          ),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(Icons.attach_money),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            affordabilityText(
+              selectedMeal.affordability,
+            ),
+          ),
+        ],
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: mealTypes(selectedMeal).length > 0
+            ? mealTypes(selectedMeal).map((type) {
+                return Text(type);
+              }).toList()
+            : [
+                Text('Unavailable'),
+              ],
+      ),
+    ];
 
     return Scaffold(
       body: SafeArea(
         top: false,
         child: CustomScrollView(
           slivers: <Widget>[
-            sliverAppBar,
+            buildSliverAppBar(mediaQuery, mealId, selectedMeal),
+            buildSliverGrid(detailContents),
             buildSliverSubHeadline(mainTheme, 'Ingredients:'),
             buildSliverList(mainTheme, selectedMeal.ingredients, 'ingredient'),
             buildSliverSizedBox(mediaQuery, 0.03),
